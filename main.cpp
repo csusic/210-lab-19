88528c0ef90b58557cea999dc9a54ebd2ef1a7bc
Christine Susic <csusic022@insite.4cd.edu>
2026-03-07 21:31:34 -0800

    Commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+// COMSC-210 | Lab 19 | Christine Susic
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+const int W1 = 8;
+
+class Movie {
+    private:
+    string movieTitle;
+    
+};
+
+int main() {
+    vector<Movie> movies; //vector
+    Movie container;
+    Movie movie1;
+    Movie movie2;
+    Movie movie3;
+    Movie movie4;
+    
+    return 0;
+}
+
